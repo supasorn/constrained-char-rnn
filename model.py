aa78bae5a71f023aa6904d92519b0c25da000b82
Supasorn <supasorn@gmail.com>
2016-02-23 00:02:30 -0800

    auto commit

--- model.py
+++ model.py
@@ -14,19 +14,23 @@ class WindowCell():
 
     @property
     def input_size(self):
-        return self._input_size
+        return self._input_size #TODO
 
     @property
     def con_size(self):
-        return self._con_size
+        return self._con_size #TODO
 
     @property
-    def input_size(self):
-        return self._input_size
+    def state_size(self):
+        return self._cluster_size #TODO
+
+    @property
+    def output_size(self):
+        return self._vocab_size
 
     # inputs - tensor of size [batch_size x vocab_size]
     # con - tensor of size [con_size x vocab_size]
-    def __call__(self, inputs, state, con):
+    def __call__(self, inputs, state, con, globalscope):
         with tf.variable_scope(type(self).__name__):
             # batch_size x 3(cluster_size)
             concat = linear(inputs, 3 * self._cluster_size, True)
@@ -35,28 +39,65 @@ class WindowCell():
             bo = tf.exp(b)
             ko = state + tf.exp(k)
 
-            wt = c
+            phi = []
+            for i in range(self._con_size):
+                # each phi is [batch_size x 1]
+                phi.append(tf.reduce_sum(ao * tf.exp(- bo * tf.square(
+                    ko - tf.constant(i, dtype=tf.float32, shape=tf.shape(k)))), 1))
 
+            # [batch_size x vocab_size]
+            wt = tf.matmul(tf.concat(1, phi), con)
+        return wt, k0
 
 
 class Network():
-    def __init__(self, model):
+    def __init__(self, cell_fn, input_size, con, hidden_unit):
+        self._con = con
+        self._con_size = tf.shape(con)[1]
+
+        self.h1 = cell_fn(hidden_unit , 1.0, input_size + self._con_size)
+        self.w = WindowCell() #TODO
+
+        self.hn = []
+        for i in range(2):
+            self.hn.append(cell_fn(hidden_unit , 1.0, input_size + hidden_unit + self.w.output_size))
+
         pass
 
     def __call__(self, inputs, state, scope=None):
-        with vs.variable_scope(scope or type(self).__name__):  # "MultiRNNCell"
-            
         cur_state_pos = 0
-        cur_inp = inputs
         new_states = []
-        for i, cell in enumerate(self._cells):
-            with tf.variable_scope("Cell%d" % i):
-                cur_state = array_ops.slice(
-                  state, [0, cur_state_pos], [-1, cell.state_size])
-                cur_state_pos += cell.state_size
-                cur_inp, new_state = cell(cur_inp, cur_state)
+
+        # state
+        # h1, w, h2, h3
+        # 2 x hidden_unit, cluster_size, 2 x hidden_unit, 2 x hidden_unit
+        with vs.variable_scope(scope or type(self).__name__):  
+            #wt_1 = ??array_ops.slice(state, [0, h1.state_size], [-1, w.state_size])
+
+            with tf.variable_scope("hidden0"):
+                cur_state = array_ops.slice(state, [0, cur_state_pos], [-1, h1.state_size])
+                cur_inp, new_state = self.h1(array_ops.concat(1, [inputs, wt_1]), cur_state)
+
                 new_states.append(new_state)
+                cur_state_pos = += h1.state_size
+
+            with tf.variable_scope("window"):
+                cur_state = array_ops.slice(state, [0, cur_state_pos], [-1, w.state_size])
+                wt, new_state = self.w(array_ops.concat(1, [cur_inp, con]), cur_state)
+
+                new_states.append(new_state)
+                cur_state_pos = += w.state_size
+
+            for i in range(len(self.hn)):
+                with tf.variable_scope("hidden%d" % (i+1)):
+                    cur_state = array_ops.slice(state, [0, cur_state_pos], [-1, self.hn[i].state_size])
+                    cur_inp, new_state = self.hn[i](array_ops.concat(1, [inputs, cur_inp, wt]), cur_state)
+                    new_states.append(new_state)
+                    cur_state_pos = += self.hn[i].state_size
+
+
         return cur_inp, array_ops.concat(1, new_states)
+
 
 class ConstrainedModel():
     def __init__(self, args, infer=False):
@@ -84,17 +125,17 @@ class ConstrainedModel():
 
         with tf.variable_scope('rnnlm'):
             softmax_w = tf.get_variable("softmax_w", [args.rnn_size, args.vocab_size])
+          
             softmax_b = tf.get_variable("softmax_b", [args.vocab_size])
-            with tf.device("/cpu:0"):
-                embedding = tf.get_variable("embedding", [args.vocab_size, args.rnn_size])
-                #embedding = tf.constant(np.identity(args.vocab_size, dtype=np.float32))
+            embedding = tf.get_variable("embedding", [args.vocab_size, args.rnn_size])
+            #embedding = tf.constant(np.identity(args.vocab_size, dtype=np.float32))
 
-                # embedding_lookup is like slicing matrix by index
-                # split along dimension 1 (0-index) with #split = seq_length
-                inputs = tf.split(1, args.seq_length, tf.nn.embedding_lookup(embedding, self.input_data))
+            # embedding_lookup is like slicing matrix by index
+            # split along dimension 1 (0-index) with #split = seq_length
+            inputs = tf.split(1, args.seq_length, tf.nn.embedding_lookup(embedding, self.input_data))
 
-                # remove dimensions with dimension 1 from the shape of tensor at the second dimension(the [1] parameter).
-                inputs = [tf.squeeze(input_, [1]) for input_ in inputs]
+            # remove dimensions with dimension 1 from the shape of tensor at the second dimension(the [1] parameter).
+            inputs = [tf.squeeze(input_, [1]) for input_ in inputs]
 
 
         def loop(prev, _):
@@ -114,11 +155,9 @@ class ConstrainedModel():
                 [tf.ones([args.batch_size * args.seq_length])],
                 args.vocab_size)
         self.cost = tf.reduce_sum(loss) / args.batch_size / args.seq_length
-        print "{}"
         print states
-        print "{}"
-        #self.final_state = states[-1]
         self.final_state = states
+
         self.lr = tf.Variable(0.0, trainable=False)
         tvars = tf.trainable_variables()
         grads, _ = tf.clip_by_global_norm(tf.gradients(self.cost, tvars),
