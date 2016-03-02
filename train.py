c86f3d9ec1a792c6038f8d05f469457699f6e0f8
Supasorn Suwajanakorn <supasorn@keech.cs.washington.edu>
2016-03-01 22:06:12 -0800

    auto commit

--- train.py
+++ train.py
@@ -38,8 +38,50 @@ def main():
     parser.add_argument('--reprocess', type=int, default=0,
                        help='reprocess input')
     args = parser.parse_args()
-    train(args)
+    #train(args)
+    train2(args)
 
+def train2(args):
+    data_loader = TextLoader(args.data_dir, args.batch_size, args.seq_length, args.reprocess)
+    args.vocab_size = data_loader.vocab_size
+
+    with open(os.path.join(args.save_dir, 'config.pkl'), 'w') as f:
+        cPickle.dump(args, f)
+    with open(os.path.join(args.save_dir, 'chars_vocab.pkl'), 'w') as f:
+        cPickle.dump((data_loader.chars, data_loader.vocab), f)
+
+    model = ConstrainedModel(args)
+
+    # reloading
+    # eta
+
+    for e in xrange(args.num_epochs):
+        print args.learning_rate * (args.decay_rate ** e)
+    exit(0)
+
+    with tf.Session() as sess:
+        tf.initialize_all_variables().run()
+        saver = tf.train.Saver(tf.all_variables())
+        for e in xrange(args.num_epochs):
+            sess.run(tf.assign(model.lr, args.learning_rate * (args.decay_rate ** e)))
+            data_loader.reset_batch_pointer()
+            state = model.initial_state.eval()
+            for b in xrange(data_loader.num_batches):
+                start = time.time()
+                x, y, con = data_loader.next_batch()
+                
+
+                feed = {model.input_data: x, model.targets: y, model.initial_state: state, model.con_data:con}
+                train_loss, state, _ = sess.run([model.cost, model.final_state, model.train_op], feed)
+                end = time.time()
+                print "{}/{} (epoch {}), train_loss = {:.3f}, time/batch = {:.3f}" \
+                    .format(e * data_loader.num_batches + b,
+                            args.num_epochs * data_loader.num_batches,
+                            e, train_loss, end - start)
+                if (e * data_loader.num_batches + b) % args.save_every == 0:
+                    checkpoint_path = os.path.join(args.save_dir, 'model.ckpt')
+                    saver.save(sess, checkpoint_path, global_step = e * data_loader.num_batches + b)
+                    print "model saved to {}".format(checkpoint_path)
 
 def train(args):
     data_loader = TextLoader(args.data_dir, args.batch_size, args.seq_length, args.reprocess)
